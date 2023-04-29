9b2a2ddf7ec0ab89d4e64eb817604ee242cebc58
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-04-29 21:04:02 +0530

    Update User.java

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -1,6 +1,30 @@
 package com.driver;
 
+import java.util.HashSet;
+
 public class User {
     private String name;
+    static HashSet<String>set;
+    public User(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
     private String mobile;
 }
