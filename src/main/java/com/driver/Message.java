f64cb24a58a948d5b06357f48ec510dc2b255612
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-05-01 11:17:07 +0530

    Update Message.java

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -5,8 +5,15 @@ import java.util.Date;
 public class Message {
     private int id;
     private String content;
+    private Date timestamp;
 
-    public Message() {
+    public Message(){
+
+    }
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
     }
 
     public int getId() {
@@ -32,21 +39,4 @@ public class Message {
     public void setTimestamp(Date timestamp) {
         this.timestamp = timestamp;
     }
-
-    public Message(int id, String content, Date timestamp) {
-        this.id = id;
-        this.content = content;
-        this.timestamp = timestamp;
-    }
-    public Message(int id) {
-        this.id = id;
-        this.timestamp = new Date();
-    }
-    public Message(int id, String content) {
-        this.id = id;
-        this.content = content;
-        this.timestamp = new Date();
-    }
-    private Date timestamp;
-
 }
