488c56cca754d91370b07419e6e3cf2393fec3df
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-05-01 11:16:22 +0530

    Update Group.java

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -8,20 +8,4 @@ public class Group {
         this.name = name;
         this.numberOfParticipants = numberOfParticipants;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getNumberOfParticipants() {
-        return numberOfParticipants;
-    }
-
-    public void setNumberOfParticipants(int numberOfParticipants) {
-        this.numberOfParticipants = numberOfParticipants;
-    }
 }
